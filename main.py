3713ed726c65ba68c180dd2ba9c00e2a92cdebbc
LucasCS21 <lucascaldeira2008@gmail.com>
2026-07-30 21:41:26 -0300

    Start simple TO-DO List

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 print("Olá! Este é meu primeiro projeto em Python!")
 
-for i in range(0, 5):
-    print("This is a simple loop.")
+tasks = []
 
-print("I love Python and Java!")
+while True:
+    choice = int(input("Digite uma opção (0 a 3): "))
+    if choice == 0:
+        break
+
+    elif choice == 1:
+        new_task = input("Digite uma tarefa para ser adicionada: ")
+        tasks.append(new_task)
+
+    elif choice == 2:
+        task = input("Digite uma tarefa para ser removida: ")
+        tasks.remove(task)
+
+    elif choice == 3:
+        for task in tasks:
+            print(task)
+
+# TODO:
+# Add colors for different priorities;
+# Switch choice 2 to an indexed version;
+# Create a menu;
+# Create functions for each choice;
+# Fix errors with try-except;
